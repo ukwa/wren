ce8a9042bb933d3775969b8b910c2113663ea41b
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-10-13 16:29:14 +0100

    Added RocksDB and rocksdbjava build.

---
+++ tinycdxserver/rocksdb-bootstrap.sh
@@ -0,0 +1,11 @@
+#git clone https://github.com/facebook/rocksdb.git
+#cd rocksdb/
+#git checkout rocksdb-3.11.2
+curl -L -O https://github.com/facebook/rocksdb/archive/rocksdb-3.11.2.tar.gz
+tar xvfz rocksdb-3.11.2.tar.gz
+cd rocksdb-rocksdb-3.11.2/
+make static_lib
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+make rocksdbjava
+make jtest
+
