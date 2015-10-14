b22c8e8a37efdf7c78135fa83e40d3c7b09fac41
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-10-14 18:10:16 +0100

    Switching Java versions between RocksDB and the tinycdxserver.

--- tinycdxserver/rocksdb-bootstrap.sh
+++ tinycdxserver/rocksdb-bootstrap.sh
@@ -1,11 +1,13 @@
 #git clone https://github.com/facebook/rocksdb.git
 #cd rocksdb/
 #git checkout rocksdb-3.11.2
-curl -L -O https://github.com/facebook/rocksdb/archive/rocksdb-3.11.2.tar.gz
-tar xvfz rocksdb-3.11.2.tar.gz
-cd rocksdb-rocksdb-3.11.2/
+curl -L -O https://github.com/facebook/rocksdb/archive/rocksdb-3.13.1.tar.gz
+tar xvfz rocksdb-3.13.1.tar.gz
+cd rocksdb-rocksdb-3.13.1/
 make static_lib
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 make rocksdbjava
 make jtest
+make install
+cd ..
 
--- tinycdxserver/tinycdxserver-bootstrap.sh
+++ tinycdxserver/tinycdxserver-bootstrap.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/anjackson/tinycdxserver.git
 cd tinycdxserver/
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 mvn package
