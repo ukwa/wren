745a52462844e82236c630d4949ef9c2ce92ed7e
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-10-15 12:05:55 +0100

    Updated info and compression seems to be on.

--- tinycdxserver/README.md
+++ tinycdxserver/README.md
@@ -4,4 +4,6 @@
      docker run -p 18080:8080 -P -v ../tinycdxserver-data:/data anjackson/tinycdxserver
 
 
+<https://gist.github.com/ato/b2ad8e65b35afe690921>
+
 
--- tinycdxserver/tinycdxserver-bootstrap.sh
+++ tinycdxserver/tinycdxserver-bootstrap.sh
@@ -1,5 +1,8 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
 git clone https://github.com/anjackson/tinycdxserver.git
 cd tinycdxserver/
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
 mvn package || exit
+
 cp target/tiny*.jar tinycdxserver.jar
