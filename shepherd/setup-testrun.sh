8f0bbce5c3fac37853a940903f18c49fe3fc52fd
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-02-10 23:35:24 +0000

    Now using improved target watching and document processing workflow.

--- shepherd/setup-testrun.sh
+++ shepherd/setup-testrun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export AMQP_URL="amqp://guest:guest@192.168.99.100:5672/%2f"
+
 # Set up a target
 ./python-shepherd/agents/w3act.py add-target --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW "https://www.gov.uk/government/publications?departments[]=department-for-transport" "Department for Transport publications"
 
@@ -10,8 +12,8 @@
 ./python-shepherd/agents/w3act.py watch-target --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW 1
 
 # Add a Document (not needed here ATM)
-#/python-shepherd/agents/w3act.py add-document --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW 1 20160202235322 "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/492092/supplementary-guidance-january-2016.pdf" "https://www.gov.uk/government/publications/department-for-transport-delivers-more-grant-funding-to-transport-freight-by-rail"
+#./python-shepherd/agents/w3act.py add-document --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW 1 20160202235322 "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/492092/supplementary-guidance-january-2016.pdf" "https://www.gov.uk/government/publications/department-for-transport-delivers-more-grant-funding-to-transport-freight-by-rail"
 
 # Launch
-./python-shepherd/agents/launcher.py --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW --amqp-url $AMQP_URL --timestamp "2016-01-13 09:00:00" FC-2-uris-to-render
+#./python-shepherd/agents/launcher.py --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW --amqp-url $AMQP_URL --timestamp "2016-01-13 09:00:00" FC-2-uris-to-render
 
