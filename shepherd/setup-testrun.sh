c55486fc8a6817d96f731f05d7c23cc43a655b5d
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-02-29 14:12:05 +0000

    Fix issues with hop precedence exchange caps for hop depth limits.

--- shepherd/setup-testrun.sh
+++ shepherd/setup-testrun.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export W3ACT_URL=http://localhost:9000/act/
+export W3ACT_USER="wa-sysadm@bl.uk"
+export W3ACT_PW=sysAdmin
 export AMQP_URL="amqp://guest:guest@192.168.99.100:5672/%2f"
 
 # Set up a target
@@ -12,7 +15,7 @@ export AMQP_URL="amqp://guest:guest@192.168.99.100:5672/%2f"
 ./python-shepherd/agents/w3act.py watch-target --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW 1
 
 # Add a Document (not needed here ATM)
-#./python-shepherd/agents/w3act.py add-document --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW 1 20160202235322 "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/492092/supplementary-guidance-january-2016.pdf" "https://www.gov.uk/government/publications/department-for-transport-delivers-more-grant-funding-to-transport-freight-by-rail"
+./python-shepherd/agents/w3act.py add-document --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW 1 20160202235322 "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/492092/supplementary-guidance-january-2016.pdf" "https://www.gov.uk/government/publications/department-for-transport-delivers-more-grant-funding-to-transport-freight-by-rail"
 
 # Launch
 #./python-shepherd/agents/launcher.py --w3act-url $W3ACT_URL --w3act-user $W3ACT_USER --w3act-pw $W3ACT_PW --amqp-url $AMQP_URL --timestamp "2016-01-13 09:00:00" FC-2-uris-to-render
